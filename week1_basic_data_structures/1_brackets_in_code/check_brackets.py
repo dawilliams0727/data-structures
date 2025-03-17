8919764c6b8b4a144d5f32876ee5460388bfd08d
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-03-17 11:47:51 -0400

    finished check brackets

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -11,20 +11,30 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    opening_brackets_index = []
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(next)
+            opening_brackets_index.append(i)
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
-
+            if len(opening_brackets_stack) == 0:
+                return str(i+1)
+            # check if popped matches the current characters compliment
+            if not are_matching(opening_brackets_stack[-1], next):
+                return str(i+1)
+            else:
+                opening_brackets_stack.pop()
+                opening_brackets_index.pop()
+            
+    return "Success" if len(opening_brackets_stack) == 0 else str(opening_brackets_index[-1] + 1)
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
