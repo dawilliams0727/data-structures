cd8ba7c54335de31ecbf52eca11105b11883629c
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-03-18 10:21:04 -0400

    solution for stack with max

--- week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -4,17 +4,27 @@ import sys
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.__max_stack = []
 
     def Push(self, a):
         self.__stack.append(a)
+        if not self.__max_stack:
+            self.__max_stack.append(a)
+        else:
+            if self.__max_stack[-1] <= a:
+                self.__max_stack.append(a)
 
     def Pop(self):
         assert(len(self.__stack))
+        if self.__max_stack[-1] == self.__stack[-1]:
+            self.__max_stack.pop()
         self.__stack.pop()
 
     def Max(self):
-        assert(len(self.__stack))
-        return max(self.__stack)
+        if self.__max_stack:
+            return self.__max_stack[-1]
+        else:
+            return
 
 
 if __name__ == '__main__':
